d838692bccbf9161a184cc989e702c22a5c3c540
Isaac <Isaaczhang11@gmail.com>
2020-03-17 23:48:14 -0600

    markdown fixed

--- src/UserSelf.js
+++ src/UserSelf.js
@@ -9,6 +9,7 @@ import AuthorProfile from './components/AuthorProfile'
 import {reactLocalStorage} from 'reactjs-localstorage';
 import './UserSelf.css';
 import cookie from 'react-cookies';
+import ReactMarkdown from 'react-markdown';
 import validateCookie from './utils/utils.js';
 import {POST_API,AUTHOR_API,CURRENT_USER_API} from "./utils/constants.js";
 
@@ -178,7 +179,7 @@ class UserSelf extends React.Component {
                             title={<a href={"/author/".concat(item.author).concat("/posts")} style={{color: '#031528'}}>{item.author}</a>}
                             description={item.published}
                       />
-                      {item.content}                      
+                      {item.contentType == "text/markdown" ? (<ReactMarkdown source = {item.content}/>) : item.content}                     
                       </List.Item>
                   )}
               />
